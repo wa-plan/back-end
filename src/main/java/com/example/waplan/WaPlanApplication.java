50cecc376012eed6be1723f8884935c3047c0c32
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-01-14 01:49:31 +0900

    init: 초기 설정

---
+++ src/main/java/com/example/waplan/WaPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.waplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaPlanApplication.class, args);
+	}
+
+}
