7c554082b1bbdd892fcde8c4f0a469bdea8191c0
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-03-15 20:46:28 +0900

    feat: goal repository 추가 (#9)

---
+++ src/main/java/com/example/waplan/repository/SecondGoalRepository.java
@@ -0,0 +1,8 @@
+package com.example.waplan.repository;
+
+import com.example.waplan.domain.SecondGoal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SecondGoalRepository extends JpaRepository<SecondGoal, Long> {
+
+}
