d7de1bc914e1a96490100adde40c7ddf761236b8
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-06-26 05:52:16 +0900

    feat: 현재 로그인 된 유저 조회

--- src/main/java/com/example/waplan/user/api/UserApi.java
+++ src/main/java/com/example/waplan/user/api/UserApi.java
@@ -1,10 +1,13 @@
 package com.example.waplan.user.api;
 
+import com.example.waplan.security.CurrentUser;
 import com.example.waplan.user.application.UserService;
 import com.example.waplan.user.application.dto.MorningAlarmRequest;
+import com.example.waplan.user.application.dto.UserResponse;
 import com.example.waplan.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +18,18 @@ public class UserApi {
 
     private final UserService userService;
 
-    @PutMapping("/users/morning_alarm")
+    @PutMapping("/user/morning_alarm")
     public ResponseEntity<Void> updateDescription(
         final User user,
         @RequestBody final MorningAlarmRequest morningAlarmRequest
     ) {
         userService.updateMorningAlarm(user, morningAlarmRequest);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getCurrentUser(@CurrentUser User user) {
+        return ResponseEntity.ok(UserResponse.of(user));
     }
 
 }
---
+++ src/main/java/com/example/waplan/user/application/dto/UserResponse.java
@@ -0,0 +1,43 @@
+package com.example.waplan.user.application.dto;
+
+import com.example.waplan.user.domain.AlarmType;
+import com.example.waplan.user.domain.Role;
+import com.example.waplan.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class UserResponse {
+
+    private Long id;
+
+    private String userId;
+
+    private String password;
+
+    private String email;
+
+    private String phoneNum;
+
+    private String description;
+
+    private Role role;
+
+    private AlarmType morningAlarm;
+
+    private AlarmType nightAlarm;
+
+    private String nickname;
+
+    public static UserResponse of(User user) {
+        return new UserResponse(user.getId(), user.getUserId(), user.getPassword(), user.getEmail(), user.getPhoneNum(), user.getDescription(), user.getRole(), user.getMorningAlarm(), user.getNightAlarm(), user.getNickname());
+    }
+}
