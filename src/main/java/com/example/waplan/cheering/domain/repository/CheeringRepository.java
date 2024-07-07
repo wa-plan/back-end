fdb8e4d543ad78730751e3929de30829270ae073
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-07-07 23:50:10 +0900

    refactor: cheering 구조 변경

---
+++ src/main/java/com/example/waplan/cheering/domain/repository/CheeringRepository.java
@@ -0,0 +1,8 @@
+package com.example.waplan.cheering.domain.repository;
+
+import com.example.waplan.cheering.domain.Cheering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheeringRepository extends JpaRepository<Cheering, Long> {
+
+}
