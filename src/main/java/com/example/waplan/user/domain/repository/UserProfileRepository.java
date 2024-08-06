0b86a1c04b944c8fdc74932ecdebf1aafdb714b8
hyeonsoo0625 <hyeonsoo02@gmail.com>
2024-08-07 02:41:01 +0900

    feat: userprofilerepository 삭제

--- src/main/java/com/example/waplan/user/domain/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.waplan.user.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-    UserProfile findByUserId(Long userId);
-}
